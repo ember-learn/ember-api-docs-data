d6d85301d732da084a35233d57b9f49ad1cb36f0
Chris Manson <chris@manson.ie>
2023-09-26 21:13:37 +0100

    update copy-entries script to do multiple versions

--- copy-entries.mjs
+++ copy-entries.mjs
@@ -1,26 +1,76 @@
-import { rmSync } from 'fs';
+import { rmSync, readdirSync } from 'fs';
 
 import fse from 'fs-extra';
 import { program } from 'commander';
 import { execa } from 'execa';
+import semver from 'semver';
+import cmp from 'semver-compare';
 
 const { copySync } = fse;
 
 program
   .argument('project')
-  .argument('version');
+  .argument('[version]')
+  .option('-m, --min-version [version]');
 
 program.parse();
 
-
 const [project, version] = program.args;
 
+const { minVersion } = program.opts();
 
-rmSync(`json-docs/${project}/${version}`, { recursive: true, force: true });
-rmSync(`rev-index/${project}-${version}.json`);
+const oldDocsPath = '/Users/mansona/temp/old-docs'
 
-copySync(`/Users/mansona/temp/old-docs/json-docs/${project}/${version}`, `./json-docs/${project}/${version}`, {recursive: true});
-copySync(`/Users/mansona/temp/old-docs/rev-index/${project}-${version}.json`, `./rev-index/${project}-${version}.json`);
 
-await execa('npm', ['run', 'fix:files'], { stdio: 'inherit'});
-await execa('npm', ['run', 'test'], { stdio: 'inherit'});
+if (version) {
+  // only apply for the specified version
+  copyEntries(project, version);
+} else {
+  // do all versions
+  const fullProjectVersions = readdirSync(
+    `${oldDocsPath}/json-docs/${project}`
+  ).filter((v) => v.match(/\d+\.\d+\.\d+/));
+  
+  const projectVersions = fullProjectVersions.map((v) => {
+    let [, major, minor] = v.match(/(\d+)\.(\d+)\.\d+/);
+    return `${major}.${minor}`;
+  });
+  
+  const uniqueProjectVersions = [...new Set(projectVersions)];
+  const highestPatchVersions = uniqueProjectVersions.map((uniqVersion) => {
+    const sortedPatchVersions = fullProjectVersions
+      .filter((projectVersion) => {
+        return semver.satisfies(projectVersion, uniqVersion);
+      })
+      .sort(cmp);
+  
+    return sortedPatchVersions[sortedPatchVersions.length - 1];
+  })
+  .sort(cmp)
+  .filter((version) => {
+    if (minVersion) {
+      return semver.gte(version, minVersion);
+    } else {
+      return true;
+    }
+  });
+
+  for (let version of highestPatchVersions) {
+    await copyEntries(project, version)
+  }
+}
+
+async function copyEntries(project, version) {
+  rmSync(`json-docs/${project}/${version}`, { recursive: true, force: true });
+  try {
+    rmSync(`rev-index/${project}-${version}.json`);
+  } catch {
+    // ignore
+  }
+  
+  copySync(`${oldDocsPath}/json-docs/${project}/${version}`, `./json-docs/${project}/${version}`, {recursive: true});
+  copySync(`${oldDocsPath}/rev-index/${project}-${version}.json`, `./rev-index/${project}-${version}.json`);
+  
+  await execa('npm', ['run', 'fix:files'], { stdio: 'inherit'});
+  await execa('npm', ['run', 'test'], { stdio: 'inherit'});
+}
